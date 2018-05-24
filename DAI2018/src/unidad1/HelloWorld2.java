90b2e1f686d2cfac8ff4fec26d6b80d5f256826a
Joel <Joel@Pankeke>
2018-05-24 12:16:22 -0500

    HelloWorld

---
+++ DAI2018/src/unidad1/HelloWorld2.java
@@ -0,0 +1,35 @@
+package unidad1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld2 extends JFrame
+{
+	private JLabel lbl_mensaje;
+	private JButton btn_aceptar;
+	
+	public HelloWorld2()
+	{
+		super("HelloWorld2");
+		setLayout(null);
+		setSize(200,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		lbl_mensaje=new JLabel("aqui esta la etiqueta");
+		lbl_mensaje.setBounds(50, 10, 150, 30);
+		add(lbl_mensaje);
+		
+		btn_aceptar=new JButton("aceptar");
+		btn_aceptar.setBounds(70, 40, 100, 30);
+		add(btn_aceptar);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) 
+	{
+		HelloWorld2 ventana=new HelloWorld2();
+		
+	}
+
+}
---
+++ DAI2018/src/unidad1/HelloWorld3.java
@@ -0,0 +1,26 @@
+package unidad1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloWorld3 extends JFrame implements ActionListener
+{
+	private JLabel lbl_mensaje;
+	private JButton btn_aceptar;
+	
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		
+		
+	}
+
+}
